e57a98e8f4d8ed09451ddc32c8033d2b83769fce
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-09-07 13:15:07 +0530

    Update Day_of_the_Week.java

--- Day_of_the_Week.java
+++ Day_of_the_Week.java
@@ -1,4 +1,4 @@
-import java.awt.*                                               ;
+import java.awt.*                                                ;
 import java.applet.*; 
 import java.awt.event.*;
 public class Day_of_the_Week extends Applet implements ActionListener
